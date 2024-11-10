a2569faae40ed5b1c925ec89aa80dde62c649eec
Kaian Samuel <kaiansamuelster@gmail.com>
2024-11-10 09:49:55 -0300

    Streams

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -7,18 +7,18 @@ class OneToHundredStream  extends Readable {
     index = 1
     
     _read(){
-        const i = this.index++
+      const i = this.index++
 
-       setTimeout(() => {
-         if(i > 100){
-            this.push(null)
-         } else {
-            const buf = Buffer.from(String(i))
-            this.push(buf)
-         } 
-       }, 1000
-       )
-    }
+      setTimeout(() => {
+        if(i > 100){
+          this.push(null)
+        } else {
+          const buf = Buffer.from(String(i))
+          this.push(buf)
+        } 
+      }, 1000
+    )
+  }
 }
 
 class InverseNumberStream extends Transform {
